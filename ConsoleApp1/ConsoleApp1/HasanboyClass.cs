a3405edccc5558af75263054790f9c3c78e5e6d3
HasanboyXomidov <hasanboyxomidov04@gmail.com>
2023-08-05 10:31:15 +0500

    jknjnkjjknjk

---
+++ ConsoleApp1/ConsoleApp1/HasanboyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class HasanboyClass
+    {
+        public static string GetHasanboy()
+        {
+            return "Hasanboy";
+        }
+    }
+}
